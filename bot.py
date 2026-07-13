2ad3bfa0e47879199bd7643dcf648b4fe39b9760
rusilka1977 <rkdskfn1977@gmail.com>
2026-07-13 19:05:36 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -170,13 +170,11 @@ async def monitor_gamelist():
 
     
 
-    headers = {
-
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, http/1.1)",
-
-        "Accept": "application/json, text/plain, */*"
-
-    }
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7"
+}
 
 
 
